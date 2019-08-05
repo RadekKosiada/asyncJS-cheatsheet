5b2ad7e9b363e6631d8c8a024a1fdb607b2633b4
RadekKosiada <kosiadar@gmail.com>
2019-08-05 21:55:22 +0200

    Added a new example

--- promises.js
+++ promises.js
@@ -31,3 +31,29 @@ function createPost(post, callback) {
 // .then(getPosts)
 // // if error is true, than it will log 'Ooops'
 // .catch(err=> console.log(err))
+
+
+//ANOTHER EXAMPLE
+
+function sleep(amount) {
+  return new Promise((resolve, reject) => {
+    if(amount <= 300) {
+      return reject ("That's too fast");
+    }
+    setTimeout(()=> resolve(`Slept for ${amount}`), amount)
+  })
+}
+
+sleep(500)
+.then((result) => {
+  console.log(result);
+  return sleep(1002);
+})
+.then((result) => {
+  console.log(result);
+  return sleep(1500);
+})
+.then((result) => {
+  console.log(result);
+  console.log("Awake!")
+})
